c4315892fe889bf0f4aeaadef38adb0f4fffddfd
sanikdah <michaelfgarofalo@gmail.com>
2021-08-14 13:07:11 -0700

    Add some stuff and rename some files.

---
+++ SM64-Android-Port/Build on PC/SM64-Android-Port.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if
+    test -f "~/SM64-Android-Port.sh"; then
+    echo "The file is in the home directory, proceeding."
+
+        else if
+            test -f "SM64-Android-Port.sh"; then
+            cp SM64-Android-Port.sh ~/SM64-Android-Port.sh
+            echo "Fixed an issue, make sure that the file
+is run from your home directory next time!"
+            chmod +x ~/SM64-Android-Port.sh
+            ~/SM64-Android-Port.sh
+                else
+                    echo "Couldn't find the file in the home directory or the current directory!
+                    Please make sure that you didn't rename the file and try again."
+fi
---
+++ SM64-Android-Port/README.md
@@ -0,0 +1,11 @@
+# Instructions
+
+## Building on Android
+
+Coming soon!
+
+## Building on PC
+
+1. Open "SM64-Android-Port.sh" from the Build on PC directory.  
+
+2. Fix
