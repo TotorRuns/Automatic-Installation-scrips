f6c8692b07e7b2a937a50cf2af2afda8aca9249e
sanikdah <michaelfgarofalo@gmail.com>
2021-08-14 13:35:18 -0700

    Update SM64-Android-Port.sh
    Stuff broke, so I fixed it.

--- SM64-Android-Port/Build on PC/SM64-Android-Port.sh
+++ SM64-Android-Port/Build on PC/SM64-Android-Port.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 if
-    test -f "~/SM64-Android-Port.sh"
+    test -f "SM64-Android-Port.sh"
 then
-    echo "The file is in the home directory, proceeding."
-
+    echo "File exists!"
 else
-    if
-        test -f "SM64-Android-Port.sh"
-    then
-        cp SM64-Android-Port.sh ~/SM64-Android-Port.sh
-        echo "Fixed an issue, make sure that the file
+    cp SM64-Android-Port.sh ~/SM64-Android-Port.sh
+    echo "Fixed an issue, make sure that the file
 is run from your home directory next time!"
-        chmod +x ~/SM64-Android-Port.sh
-        ~/SM64-Android-Port.sh
-    else
-        echo "Couldn't find the file in the home directory or the current directory!
-Please make sure that you didn't rename the file and try again."
-    fi
+    chmod +x ~/SM64-Android-Port.sh
+    ~/SM64-Android-Port.sh
 fi
